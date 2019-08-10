e57bd7de7a85bd935ff2f80972f1cd140a378291
SphinxNumberNine <ashwathkrishnan@yahoo.com>
2019-08-10 11:39:23 -0400

    More heroku fixes

--- index.js
+++ index.js
@@ -6,5 +6,18 @@ app.use(express.urlencoded({ extended: false }));
 
 require("./routes/contactRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static("website/build"));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "website", "build", "index.html"));
+  });
+}
+
 const port = process.env.port || 5000;
 app.listen(port);
